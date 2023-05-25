d286d45f4413bbaac2c99ae760dc717c5cd7ab19
BranBayou <berhan.baye@gmail.com>
2023-05-25 23:41:23 +0300

    connet external api

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,26 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/uMfxud1McryQAfRNygOp/books';
+
+export const addNewBook = createAsyncThunk('books/addNewBook', async (book) => {
+  try {
+    await axios.post(`${baseUrl}/books`, book);
+  } catch (error) {
+    throw new Error(error);
+  }
+  return book;
+});
+
+export const getBooks = createAsyncThunk('books/getBooks', async () => {
+  try {
+    const response = await axios.get(baseUrl);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+});
+
+export const deleteBook = createAsyncThunk('books/DELETE_BOOK', async (id) => {
+  try {
+    await axios.delete(`${baseUrl}/books/${id}`);
+  } catch (error) {
+    throw new Error(error);
+  }
+  return id;
+});
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
+  loading: false,
+  error: null,
 };
 
 export const bookSlice = createSlice({
@@ -28,15 +41,37 @@ export const bookSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.books = [...state.books, {
-        id: Math.floor(Math.random() * 100),
-        title: action.payload.title,
-        author: action.payload.author,
-      }];
+      state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      const bookId = action.payload;
-      state.books = state.books.filter((book) => book.item_id !== bookId);
+      const newBooksState = { ...state };
+      newBooksState.books = state.books.filter((book) => book.item_id !== action.payload);
+      return newBooksState;
+    },
+  },
+  extraReducers: {
+    [getBooks.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [getBooks.fulfilled]: (state, action) => {
+      const books = Object.keys(action.payload).map(
+        (key) => (
+          {
+            item_id: key,
+            title: action.payload[key][0].title,
+            author: action.payload[key][0].author,
+          }),
+      );
+      return ({
+        ...state,
+        loading: false,
+        books,
+      });
+    },
+    [getBooks.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
     },
   },
 });
