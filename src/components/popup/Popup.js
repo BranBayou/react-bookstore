33ecc661a7b06d1ca7fc59bd9ccb45580a2059e9
BranBayou <berhan.baye@gmail.com>
2023-06-13 22:07:07 +0300

    Add popup

---
+++ src/components/popup/Popup.js
@@ -0,0 +1,14 @@
+function Popup() {
+  return (
+    <div className="popup-con">
+      <div className="popup-top">
+        <p>Computer</p>
+      </div>
+      <div className="popup-bottom">
+        <p>comments</p>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
